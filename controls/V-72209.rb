f87ee84e6dbd049b12abd8f507e5f395d9d794c1
Mo Shark <HackerShark@users.noreply.github.com>
2020-09-10 12:28:11 -0400

    Fixes #115, fixes #25 (#124)
    * Fixes #115, fixes #25
    
    Signed-off-by: HackerShark <melsharkawi@mitre.org>
    
    * Updating profile.json in the repository
    
    * Revert "Fixes #111 (#118)"
    
    This reverts commit e53c5a4f39582120c93d603fd816130fcc165127.
    
    * Switch over to the official ruby setup since the actions/setup-ruby one is throwing an error
    
    * Lock ruby version to 2.6 in order to avoid Puppet load error
    
    bundler is installed automatically, no need to install it as part of our steps
    
    Release Puppet version lock
    
    * Updating profile.json in the repository
    
    * Update V-72417.rb
    
    bugfix
    
    * Add Trend AV to V-72213 checks
    
    Trend Micro Anti-virus service is named 'ds_agent'.
    This adds one more type of anti-virus this check can check for.
    
    * Updating profile.json in the repository
    
    * Fix describe.one formatting for V-72213
    
    * Updating profile.json in the repository
    
    * Fixes #115, fixes #25
    
    Signed-off-by: HackerShark <melsharkawi@mitre.org>
    
    * Updating profile.json in the repository
    
    * Fixes #115, fixes #25
    
    Signed-off-by: HackerShark <melsharkawi@mitre.org>
    
    * rebase, resolve merge conflicts
    
    Signed-off-by: HackerShark <melsharkawi@mitre.org>
    
    * Updating profile.json in the repository
    
    * Updating profile.json in the repository
    
    Co-authored-by: Robert Clark <rbclark@mitre.org>
    Co-authored-by: rbclark <rbclark@users.noreply.github.com>
    Co-authored-by: Maven35 <Charles.Tony.Gibbs@gmail.com>
    Co-authored-by: ejaronne <34140975+ejaronne@users.noreply.github.com>
    Co-authored-by: ejaronne <ejaronne@users.noreply.github.com>

--- controls/V-72209.rb
+++ controls/V-72209.rb
@@ -43,10 +43,15 @@ aggregation system:
   tag cci: ["CCI-000366"]
   tag nist: ["CM-6 b", "Rev_4"]
 
-  log_pkg_path = input('log_pkg_path')
 
-  describe command("grep @ #{log_pkg_path} | grep -v \"^#\"") do
-    its('stdout.strip') { should_not be_empty }
+  if input('alternate_logs')
+    describe "An alternate logging system is used. This test cannot be checked in a automated fashion and you must check it manually" do
+      skip "An alternate logging system is used. This check must be performed manually"
+    end
+  else
+    describe command("grep @ #{input('log_pkg_path')} | grep -v \"^#\"") do
+      its('stdout.strip') { should_not be_empty }
+    end
   end
 end
 
